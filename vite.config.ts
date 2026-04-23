3100a97ab3cfe1cc5117d61db5f5747cdc585dfb
jairdeyner <jairdeyner730791@gmail.com>
2026-04-22 19:42:21 -0500

    chore(config): setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
 });
